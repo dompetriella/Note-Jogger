02e34e86201fcb4d147d924282414e95d6700888
Dominic Petriella <dpetriella@magicwrighter.com>
2023-06-03 21:52:29 -0400

    added 12 count quiz gen

--- lib/game_logic/quiz_generate.dart
+++ lib/game_logic/quiz_generate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:note_jogger/globals.dart';
 import 'package:note_jogger/provider.dart';
 import 'dart:math';
 import '../components/quiz/quiz_generate.dart';
@@ -9,14 +10,36 @@ import '../models/notes.dart';
 createNewQuizGenerateList(WidgetRef ref, List<Enum> clefNotes,
     {int numberOfButtons = 5}) {
   ref.watch(quizStagingProvider.notifier).state = [];
+  List<Enum> preliminaryNoteList = [];
+  if (GLOBAL_normal_quiz_amount >= clefNotes.length) {
+    // less than normal quiz amount
+    for (var note in clefNotes) {
+      preliminaryNoteList.add(note);
+    }
+    int notesLeft = preliminaryNoteList.length;
+    for (var i = 0; i < GLOBAL_normal_quiz_amount - notesLeft; i++) {
+      final random = new Random();
+      var index = random.nextInt(clefNotes.length);
+      var note = clefNotes[index];
+      preliminaryNoteList.add(note);
+    }
+    // more than normal quiz amount
+  } else {
+    final random = new Random();
+    var index = random.nextInt(clefNotes.length);
+    var note = clefNotes[index];
+    if (!preliminaryNoteList.contains(note)) {
+      preliminaryNoteList.add(note);
+    }
+  }
 
-  clefNotes.shuffle();
-  for (var enumNote in clefNotes) {
+  for (var enumNote in preliminaryNoteList) {
     ref
         .watch(quizStagingProvider.notifier)
         .state
         .add(QuizGenerate(note: enumNote, numberOfButtons: numberOfButtons));
   }
+  print(ref.read(quizStagingProvider).length);
   ref.read(livesProvider.notifier).resetLives();
 }
 
--- lib/globals.dart
+++ lib/globals.dart
@@ -4,4 +4,7 @@ double GLOBAL_bRankTimeLimit = 3500;
 double GLOBAL_cRankTimeLimit = 5000;
 
 int GLOBAL_lives = 3;
+
+int GLOBAL_normal_quiz_amount = 12;
+
 double GLOBAL_heartBonus = 150;

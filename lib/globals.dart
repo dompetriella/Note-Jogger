bfe7ce1c8246c999a743a0d27d6b611a60550978
dompetriella <dompetriella@gmail.com>
2023-06-18 01:19:46 -0400

    added new globals for path

--- lib/globals.dart
+++ lib/globals.dart
@@ -8,3 +8,6 @@ int GLOBAL_lives = 3;
 int GLOBAL_normal_quiz_amount = 10;
 
 double GLOBAL_heartBonus = 150;
+
+String GLOBAL_treble_clef_path = 'assets/treble_clef.svg';
+String GLOBAL_bass_clef_path = 'assets/bass_clef.svg';

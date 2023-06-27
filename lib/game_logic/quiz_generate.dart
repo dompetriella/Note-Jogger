79c89269596d14f172e5f8260a80f65dc752c0aa
Dominic Petriella <dpetriella@magicwrighter.com>
2023-06-27 15:19:30 -0400

    made a horrible fix, but a fix all the same

--- lib/game_logic/quiz_generate.dart
+++ lib/game_logic/quiz_generate.dart
@@ -82,6 +82,46 @@ List<Enum> trimClefNotes(List<Enum> clef, Enum lowestNote, Enum highestNote,
     bool spaceNotesOnly = false,
     bool includeFlats = true}) {
   List<Enum> returnList = [];
+  // TODO: this is a hacky way to do this, need to change
+  bool isTrebleClef = clef.first.name[0] == 'C';
+  if (isTrebleClef) {
+    if (lineNotesOnly) {
+      return [
+        TrebleClefNotes.E4,
+        TrebleClefNotes.G4,
+        TrebleClefNotes.B4,
+        TrebleClefNotes.D5,
+        TrebleClefNotes.F5
+      ];
+    }
+    if (spaceNotesOnly) {
+      return [
+        TrebleClefNotes.F4,
+        TrebleClefNotes.A4,
+        TrebleClefNotes.C5,
+        TrebleClefNotes.E5
+      ];
+    }
+  } else {
+    if (lineNotesOnly) {
+      return [
+        BassClefNotes.G3,
+        BassClefNotes.B3,
+        BassClefNotes.D3,
+        BassClefNotes.F4,
+        BassClefNotes.A4
+      ];
+    }
+    if (spaceNotesOnly) {
+      return [
+        BassClefNotes.A3,
+        BassClefNotes.C3,
+        BassClefNotes.E3,
+        BassClefNotes.G4,
+      ];
+    }
+  }
+
   for (var i = 0; i < clef.length; i++) {
     if (i >= lowestNote.index && i <= highestNote.index) {
       returnList.add(clef[i]);
@@ -95,5 +135,6 @@ List<Enum> trimClefNotes(List<Enum> clef, Enum lowestNote, Enum highestNote,
         )
         .toList();
   }
+
   return returnList;
 }

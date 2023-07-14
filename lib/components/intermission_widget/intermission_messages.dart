9ca30a51f020789ed2a1cd679aab413365a3862c
Dom <dompetriella@gmail.com>
2023-07-13 23:56:58 -0400

    formatting issues for messaging

--- lib/components/intermission_widget/intermission_messages.dart
+++ lib/components/intermission_widget/intermission_messages.dart
@@ -12,12 +12,12 @@ List<String> topMessage = [
 
 List<String> bottomMessage = [
   "Don't worry, I'll get the water",
-  "All that hard work pays off!"
-      "Bottoms up!",
+  "All that hard work pays off!",
+  "Bottoms up!",
   "How about an ice cold results screen?",
   "Survey says....",
   "Take a breather!",
   "Let's see how you did!",
-  "How's this look?"
-      "Let's fill out some results"
+  "How's this look?",
+  "Let's fill out some results"
 ];
